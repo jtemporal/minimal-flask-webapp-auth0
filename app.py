3734dbe21c6f7c95cbb8ec1f0d1b9f408d94b4ac
Jessica Temporal <jessicatemporal@gmail.com>
2023-12-14 13:49:07 -0500

    Registers auth blueprint

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 
+from auth.views import auth_bp
 from config import config
 from webapp.views import webapp_bp
 
@@ -7,4 +8,5 @@ from webapp.views import webapp_bp
 app = Flask(__name__)
 app.secret_key = config["WEBAPP"]["SECRET_KEY"]
 
+app.register_blueprint(auth_bp, url_prefix='/')
 app.register_blueprint(webapp_bp, url_prefix='/')
