63b86059f409eee714a79f1f4c8e6da4bd2615e5
Jessica Temporal <jessicatemporal@gmail.com>
2023-12-14 13:51:28 -0500

    adds profile endpoint to webapp

--- webapp/views.py
+++ webapp/views.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from auth.decorators import requires_auth
+from flask import Blueprint, render_template, session
 
 
 webapp_bp = Blueprint('webapp', __name__, template_folder="templates")
@@ -11,3 +12,15 @@ def home():
     """
 
     return render_template("home.html")
+
+
+@webapp_bp.route("/profile")
+@requires_auth
+def profile():
+    """
+    Protected endpoint which displays your profile if you are logged in, otherwise it prompts the user to log in
+    """
+    return render_template(
+        'profile.html',
+        session=session.get('user')
+    )
